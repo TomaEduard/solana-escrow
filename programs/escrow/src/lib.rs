9ef619160fb813f9a7dcdb58cc52d581cd8a0ade
Eduard <eduard.daniel.toma@gmail.com>
2024-07-30 15:31:10 +0300

    course-9-using Anchor with tokens

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -2,9 +2,7 @@ pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod state;
-
 use anchor_lang::prelude::*;
-
 pub use constants::*;
 pub use instructions::*;
 pub use state::*;
@@ -14,8 +12,13 @@ declare_id!("7FXbuQWwz52MgX9KswS7PgXJmU4AoeanLMiJ7annGgVk");
 #[program]
 pub mod escrow {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
     }
 }
